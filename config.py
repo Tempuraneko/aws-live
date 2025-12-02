cd882f5233d4c235395a56aa76d672b225b5ec69
Tempuraneko <ngjiawei0201@gmail.com>
2025-12-02 11:06:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "ngjiawei-employee"
+customregion = "us-east-1b"
+
